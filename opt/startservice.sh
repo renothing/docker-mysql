f3a0b51b25bd966a2339981e6a164ccf0171c053
renothing <frankdot@qq.com>
2016-10-09 14:50:17 +0800

    fix bug when start with datadir

--- opt/startservice.sh
+++ opt/startservice.sh
@@ -5,8 +5,9 @@ test -d /var/run/mysqld || mkdir -p /var/run/mysqld
 stat -c "%U" /var/lib/mysql|grep -q mysql || chown mysql:mysql -R /var/lib/mysql
 stat -c "%U" /var/log/mysql|grep -q mysql || chown mysql:mysql -R /var/log/mysql
 stat -c "%U" /var/run/mysqld|grep -q mysql || chown mysql:mysql -R /var/run/mysqld
-test -f /etc/mysql/my.cnf || ln -sf /opt/my.cnf /etc/mysql/my.cnf
+test -f /etc/mysql/my.cnf || cp -rf /opt/my.cnf /etc/mysql/my.cnf
 datadir=`grep ^datadir opt/my.cnf |awk '{print $NF}'`
 test -f $datadir/auto.cnf || bash /opt/init.sh	
+sed -i "s/POOLSIZE/$buffersize/g" /etc/mysql/my.cnf
 ps aux|grep -v grep|grep -q mysql || /etc/init.d/mysql start
 tail -f /var/log/mysql/error.log
