d570212bbac6bc9d7be77409414612374f9be599
renothing <frankdot@qq.com>
2017-01-06 14:24:25 +0800

    fix error when init

--- opt/startservice.sh
+++ opt/startservice.sh
@@ -8,7 +8,7 @@ stat -c "%U" /var/lib/mysql|grep -q mysql || chown mysql:mysql -R /var/lib/mysql
 stat -c "%U" /var/log/mysql|grep -q mysql || chown mysql:mysql -R /var/log/mysql
 stat -c "%U" /var/run/mysqld|grep -q mysql || chown mysql:mysql -R /var/run/mysqld
 test -f /etc/mysql/my.cnf || cp -rf /opt/my.cnf /etc/mysql/my.cnf
-datadir=`grep ^datadir opt/my.cnf |awk '{print $NF}'`
+datadir=`grep ^datadir /opt/my.cnf |awk '{print $NF}'`
 test -f $datadir/auto.cnf || sh /opt/init.sh	
 sed -i "s/innodb_buffer_pool_size=.*/innodb_buffer_pool_size=${POOLSIZE}/g" /etc/mysql/my.cnf
 rm -rf /var/run/mysqld/mysqld.pid /var/run/mysqld/mysqld.sock /var/run/mysqld/mysqld.sock.lock
